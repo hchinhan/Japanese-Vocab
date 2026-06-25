0661f853a0c28415e9146f19bea05954febdded6
hchinhan <cucmo845@gmail.com>
2026-06-25 12:02:30 +0700

    add: chuong6

---
+++ chuong6.js
@@ -0,0 +1,32 @@
+// File: chuong6.js
+const vocabChuong6 = {
+    // Nhóm 1 (V1 - Động từ đuôi ~u)
+    "Rửa": "あらう",
+    "Học": "ならう",
+    "Gặp": "あう",
+    "Mua": "かう",
+    "Đi": "いく",
+    "Viết": "かく",
+    "Nghe": "きく",
+    "Bơi": "およぐ",
+    "Nói chuyện": "はなす",
+    "Chờ, đợi": "まつ",
+    "Chơi": "あそぶ",
+    "Đọc": "よむ",
+    "Uống": "のむ",
+    "Nghỉ ngơi": "やすむ",
+    "Nấu ăn, làm ra": "つくる",
+    "Về, quay về, trở về": "かえる",
+    "Quay lại": "もどる",
+
+    // Nhóm 2 (V2 - Động từ đuôi ~iru / ~eru)
+    "Ăn": "たべる",
+    "Ngủ": "ねる",
+    "Chỉ, dạy": "おしえる",
+    "Xem, nhìn": "みる",
+    "Thức, ngủ dậy": "おきる",
+
+    // Nhóm 3 (V3 - Động từ bất quy tắc)
+    "Đến": "くる",
+    "Làm": "する"
+};
